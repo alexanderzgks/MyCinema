ac21848ba3ac520de54dfaf532227f6c2668fcf1
alexanderzgks <alexanderzgks@gmail.com>
2026-03-09 23:06:11 +0200

    Add some comments

--- src/main/java/org/example/repo/ProfileJsonRepository.java
+++ src/main/java/org/example/repo/ProfileJsonRepository.java
@@ -5,11 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.example.model.Profile;
 
-import javax.print.DocFlavor;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
 
 public class ProfileJsonRepository {
 
@@ -27,12 +25,12 @@ public class ProfileJsonRepository {
         return baseDir.resolve(code);
     }
 
-    //Create the file path for json file of the User
+    //Create the file path for JSON file of the User
     private Path userFile(String code){
         return userDir(code).resolve("profile.json");
     }
 
-
+    //Save the profile as a JSON file of the User
     public void save(Profile profile) throws IOException{
         if(profile == null || profile.getId() == null || profile.getId().isBlank()){
             throw new IllegalArgumentException("Profile/id must not be empty");
@@ -40,38 +38,19 @@ public class ProfileJsonRepository {
         Path dir = userDir(profile.getCode());
         //Create directory
         Files.createDirectories(dir);
-
         Path file = userFile(profile.getCode());
         //Create the file in the directory that I create
         mapper.writeValue(file.toFile(), profile);
     }
 
+    //Load the profile from the JSONS files
     public Profile load(String code) throws IOException{
         Path file = userFile(code);
         if(!Files.exists(file)) return null;
         return mapper.readValue(file.toFile(), Profile.class);
     }
 
-    public Profile loadUser(String code) throws IOException{
-        Path path = userFile(code);
-        if(!Files.exists(path)) return null;
-        return mapper.readValue(path.toFile(), Profile.class);
-    }
-
-    public String getEmail(String code) throws IOException{
-        Path path = userFile(code);
-        if(!Files.exists(path)){
-            return "File error - File does not exist";
-        }
-        String json = Files.readString(path);
-        JsonNode jsonNode = mapper.readTree(json);
-        String email = jsonNode.get("email").asText();
-        if(email==null){
-            return "Email error - Email is empty";
-        }
-        return email;
-    }
-
+    //Get the username
     public String getUsername(String code) throws IOException{
         Path path= userFile(code);
         if(!Files.exists(path)){
@@ -86,7 +65,23 @@ public class ProfileJsonRepository {
         return username;
     }
 
-    public boolean delete(String name,String code) throws IOException{
+    //Get the email
+    public String getEmail(String code) throws IOException{
+        Path path = userFile(code);
+        if(!Files.exists(path)){
+            return "File error - File does not exist";
+        }
+        String json = Files.readString(path);
+        JsonNode jsonNode = mapper.readTree(json);
+        String email = jsonNode.get("email").asText();
+        if(email==null){
+            return "Email error - Email is empty";
+        }
+        return email;
+    }
+
+    //Delete the file of one user
+    public boolean delete(String code) throws IOException{
         Path file = userFile(code);
         if(!Files.exists(file)) return false;
         Files.delete(file);
